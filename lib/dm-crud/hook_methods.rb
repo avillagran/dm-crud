91d4151e3e56fcc6717e46389fad3436b273af51
Andrés Villagrán <andres@villagranquiroz.cl>
2012-03-05 11:20:04 -0300

    Logger order, Successful action, Successful action destroy

--- lib/dm-crud/hook_methods.rb
+++ lib/dm-crud/hook_methods.rb
@@ -6,55 +6,66 @@ module Crud
       
       def before_create
         # just a hook!
-        puts "> Crud::before_create" if @crud_options[:log]
+        logger.info "> Crud::before_create" if @crud_options[:log]
         before_action
       end    
         
       def before_update
         # just a hook!
-        puts "> Crud::before_update" if @crud_options[:log]
+        logger.info "> Crud::before_update" if @crud_options[:log]
         before_action
       end    
         
       def before_destroy
         # just a hook!
-        puts "> Crud::before_destroy" if @crud_options[:log]
+        logger.info "> Crud::before_destroy" if @crud_options[:log]
         before_action
       end
             
       def before_action
         # just a hook!
-        puts "> Crud::before_action" if @crud_options[:log]
+        logger.info "> Crud::before_action" if @crud_options[:log]
         true
       end
       
               
       
       def successful_create
-        puts "> Crud::successful_create" if @crud_options[:log]
+        logger.info "> Crud::successful_create" if @crud_options[:log]
 
         flash[:notice] = t('crud.created', :what => set_what)
-        
+        successful_action
         after_success
       end
       
       def successful_update
-        puts "> Crud::successful_update" if @crud_options[:log]
+        logger.info "> Crud::successful_update" if @crud_options[:log]
         
         flash[:notice] = t('crud.updated', :what => set_what)
-        
+        successful_action
+
         after_success
       end
       
       def successful_destroy
-        puts "> Crud::successful_destroy" if @crud_options[:log]
-        flash.notice = t('crud.destroyed', :what => @what)
-        
+        logger.info "> Crud::successful_destroy" if @crud_options[:log]
+        flash.notice = t('crud.destroyed', :what => @what)        
+
+        successful_action_destroy
+
         after_success
       end
+
+      def successful_action
+        logger.info "> Crud::successful_action" if @crud_options[:log]
+      end
       
+      def successful_action_destroy
+        logger.info "> Crud::successful_action_destroy" if @crud_options[:log]
+      end
+
       def after_success
-        puts "> Crud::after_success" if @crud_options[:log]
+        logger.info "> Crud::after_success" if @crud_options[:log]
         
         unless request.xhr?
           if @redirect_to_url
@@ -84,25 +95,25 @@ module Crud
       
       
       def failed_create
-        puts "> Crud::failed_create" if @crud_options[:log]
+        logger.info "> Crud::failed_create" if @crud_options[:log]
         flash[:error] = t('crud.failed_create', :what => set_what)
         after_fail
       end
       
       def failed_update
-        puts "> Crud::failed_update" if @crud_options[:log]
+        logger.info "> Crud::failed_update" if @crud_options[:log]
         flash[:error] = t('crud.failed_update', :what => set_what)
         after_fail
       end
       
       def failed_destroy
-        puts "> Crud::failed_destroy" if @crud_options[:log]
+        logger.info "> Crud::failed_destroy" if @crud_options[:log]
         flash[:error] = t('crud.failed_destroy', :what => @what)
         after_fail
       end
       
       def after_fail
-        puts "> Crud::after_fail" if @crud_options[:log]
+        logger.info "> Crud::after_fail" if @crud_options[:log]
         unless request.xhr?
           render :action => request.post? ? 'new' : 'edit'
         else          
