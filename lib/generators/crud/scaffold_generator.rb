5791eb399c7c46432021654736ce989e3775bc6a
Andrés Villagrán <andres@villagranquiroz.cl>
2012-07-19 18:15:42 -0400

    Fix class verification

--- lib/generators/crud/scaffold_generator.rb
+++ lib/generators/crud/scaffold_generator.rb
@@ -8,13 +8,14 @@ module Crud
 
         argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
 
-        check_class_collision
+        
 
         class_option :timestamps, :type => :boolean
         class_option :parent,     :type => :string, :desc => "The parent class for the generated model"
 
 
         def scaffold
+            check_class_collision if create.include?('model')
 
             template 'model.rb', "app/models/#{file_name}.rb" if create.include?('model') or create.include?('all')
             template 'controller.rb', "app/controllers/#{plural_file_name}_controller.rb" if create.include?('controller') or create.include?('all')
